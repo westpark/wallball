f7be04e54228a739d92852ada589ca316c2f5ee1
Tim Golden <mail@timgolden.me.uk>
2016-11-03 20:46:22 +0000

    Add code for the ball bouncing and the bat moving

--- wallball/s2a.py
+++ wallball/s2a.py
@@ -9,5 +9,8 @@ ball = Ball(WIDTH / 2, HEIGHT / 2, 30, 30)
 ball.colour = "red"
 
 def draw():
+    #
+    # Clear the screen and place the ball at its current position
+    #
     screen.clear()
     screen.draw.filled_rect(ball, ball.colour)
