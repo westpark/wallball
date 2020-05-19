4382c52415f6bebf5bbaceed1aa8b6a9f49d3a6c
Tim Golden <mail@timgolden.me.uk>
2020-05-19 15:49:24 +0100

    Whoops. Stick to 30x30 for the ball

--- docs/steps/code/s3b.py
+++ docs/steps/code/s3b.py
@@ -5,7 +5,7 @@ class Ball(ZRect): pass
 #
 # The ball is a red square halfway across the game screen
 #
-ball = Ball(0, 0, 100, 100)
+ball = Ball(0, 0, 30, 30)
 ball.center = WIDTH / 2, HEIGHT / 2
 ball.colour = "red"
 #
