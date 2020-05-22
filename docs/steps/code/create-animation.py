609fb2d313fe786bd6e72239070729051bc12ce8
Tim Golden <mail@timgolden.me.uk>
2020-05-22 08:30:32 +0100

    Switch to 30 fps which --weirdly -- seems to be faster!

--- docs/steps/code/create-animation.py
+++ docs/steps/code/create-animation.py
@@ -17,7 +17,7 @@ def main(step):
     files = sorted(glob.glob(os.path.join(IMAGES_DIRPATH, "%s.*.png" % step)))
     try:
         frames = [imageio.imread(f) for f in files]
-        imageio.mimsave(os.path.join(IMAGES_DIRPATH, "step-%s.gif" % step), frames, "GIF", fps=90)
+        imageio.mimsave(os.path.join(IMAGES_DIRPATH, "step-%s.gif" % step), frames, "GIF", fps=30)
     except:
         raise
     else:
